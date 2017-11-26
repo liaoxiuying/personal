1ff009660157bf871a88d0f6f693991802ce00f6
Administrator <Administrator@192.168.156.2>
2017-11-26 20:19:59 +0800

    对intel网站首页的模仿

---
+++ copyIntel/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/11/4.
+ */
+window.onload=function(){
+    function collapse(btn){
+        $("button.this+ul").show();
+    }
+    function collopseMenu(){
+        $("#menuModal").show();
+        //$("#menuModal").animate("move" ,"1s","forwards");
+    }
+
+};
